4d4b3102cd6056c9f23f7c2376df02539e5b7573
keshav rana <krana1583@gmail.com>
2022-03-11 13:37:54 +1030

    Added something

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
+using namespace std;
 
 void print_scaled_matrix(int array[3][3],int scale) {
-    for(int row = 0; row < 3; row++){
-        for(int col = 0; col < 3; col++){
-            array[row][col] = array[row][col] * scale;
-            std::cout << array[row][col] << " ";
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            cout << array[i][j] * scale << " ";
         }
-        std::cout << std::endl;
+        cout << endl;
     }
 }
