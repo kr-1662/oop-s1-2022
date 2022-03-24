7fa8e5c3b9e942fa13fe343f2f2eddf96d56d9ee
keshav rana <krana1583@gmail.com>
2022-03-25 01:28:08 +1030

    dsfsdfsd

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -2,7 +2,7 @@
 
 void copy_2d_strings(std::string first[][2], std::string second[][2], int n) {
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+        for (int j = 0; j < 2; j++) {
             first[i][j] = second[i][j];
         }
     }
