819d8b7ca50417bc172443489de7544d31b4c7fd
keshav rana <krana1583@gmail.com>
2022-03-25 16:45:21 +1030

    fsddsfsd

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -9,6 +9,21 @@ int maximum_sum(int *nums,int length) {
         }
     }
 
-    
-
+    int max = *nums;
+    for (int i = 0; i < length; i++) {
+        int subLen = 1;
+        for (int j = i ; j < length; j++) {
+            int storage[subLen];
+            int sum = 0;
+            for (int k = 0; k < subLen; k++) {
+                storage[k] = *(nums+i+k);
+                sum += storage[k];
+            }
+            if(sum > max){
+                max = sum;
+            }
+            subLen++;
+        }
+    }
+    return max;
 }
--- main-2-2.cpp
+++ main-2-2.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 
+extern int maximum_sum(int *nums,int length);
+
 int main() {
-    
+    int arr[10] = {31,-41,59,26,-53,58,97,-93,-23,84};
+    int *nums = &arr[0];
+
+    cout << maximum_sum(nums, 10) << endl;
+
+    return 0;
 }
