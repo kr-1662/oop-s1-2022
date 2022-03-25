f4928abbe6341f97010b3b13fd494340192a4b62
keshav rana <krana1583@gmail.com>
2022-03-25 16:46:44 +1030

    fsdfdfs

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -8,22 +8,5 @@ int maximum_sum(int *nums,int length) {
             return 0;
         }
     }
-
-    int max = *nums;
-    for (int i = 0; i < length; i++) {
-        int subLen = 1;
-        for (int j = i ; j < length; j++) {
-            int storage[subLen];
-            int sum = 0;
-            for (int k = 0; k < subLen; k++) {
-                storage[k] = *(nums+i+k);
-                sum += storage[k];
-            }
-            if(sum > max){
-                max = sum;
-            }
-            subLen++;
-        }
-    }
-    return max;
+    
 }
