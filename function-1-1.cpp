9ae72ea7308a7e8c1bd1d1e370e976adbff9cd09
keshav rana <krana1583@gmail.com>
2022-03-30 09:46:12 +1030

    sfsdfdss

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 
+int *readNumbers() {
+    int *arr = new int[10];
+
+    for (int i = 0; i < 10; i++) {
+        std::cin >> arr[i];
+    }
+
+    delete [] arr;
+
+    return arr;
+}
+
 void printNumbers(int *numbers,int length) {
     if (length < 0) {
         std::cout << 0;
