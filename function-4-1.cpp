34b1a82439d73ada8c6e7dd77abd4847fbfde263
keshav rana <krana1583@gmail.com>
2022-04-01 16:46:05 +1030

    fsdfdssd

--- function-4-1.cpp
+++ function-4-1.cpp
@@ -1,18 +1,20 @@
+#include <iostream>
+using namespace std;
+
 int* min_max(int **vals,int num_rows, int num_cols) {
-    int arr[2] = {0,0};
-    int max_val = 50;
-    int min_val = 50;
+    int val1 = 50;
+    int val2 = 50;
+    int *max_val = &val1;
+    int *min_val = &val2;
     for (int i = 0; i < num_rows; i++) {
         for (int j = 0; j < num_cols; j++) {
-            if (max_val < vals[i][j]) {
-                max_val = vals[i][j];
-                arr[1] = max_val;
+            if (*max_val < vals[i][j]) {
+                *max_val = vals[i][j];
             }
-            if (min_val > vals[i][j]) {
-                min_val = vals[i][j];
-                arr[0] = min_val;
+            if (*min_val > vals[i][j]) {
+                *min_val = vals[i][j];
             }
         }
     }
-    return arr;
+    return min_val, max_val;
 }
--- main-4-1.cpp
+++ main-4-1.cpp
@@ -4,7 +4,10 @@ using namespace std;
 extern int* min_max(int **vals,int num_rows, int num_cols);
 
 int main() {
-    
+    int arr[2][2] = {{40,50},
+                     {1,2}};
+
+    cout << min_max(arr, 2, 2) << endl;
 
     return 0;
 }
