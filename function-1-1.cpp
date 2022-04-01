09dc4cc346937fc726ebe0c800db3281165fee86
keshav rana <krana1583@gmail.com>
2022-04-01 15:21:04 +1030

    dsfsdfsd

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int *readNumbers() {
-    int *arr = new int[10];
+int* create_ints(int num_vals) {
+    int *arr = new int[num_vals];
 
-    for (int i = 0; i < 10; i++) {
-        cin >> arr[i];
+    for (int i = 0; i < num_vals; i++) {
+        arr[i] = i;
     }
 
     return arr;
     delete [] arr;
 }
 
-void printNumbers(int *numbers,int length) {
-    if (length > 0) {
-        for (int i = 0; i < length; i++) {
-            cout << numbers[i];
+
+
+void display_array(int *vals, int num_vals) {
+    for (int i = 0; i < num_vals; i++) {
+        cout << vals[i] << " ";
     }
-  }
+    cout << endl;
 }
--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 
-extern int* readNumbers();
-extern void printNumbers(int *numbers,int length);
+extern int* create_ints(int num_vals);
+extern void display_array(int *vals, int num_vals);
 
 int main() {
-    int arr[10] = {1,2,3,4,5,6,7,8,9,10};
-    readNumbers();
-    printNumbers(arr, 10);
+    display_array(create_ints(5), 5);
+
+    return 0;
 }
