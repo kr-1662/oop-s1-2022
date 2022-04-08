8bf5a8a11db68f4836eda1395289a5b8bbfcb4e5
keshav rana <krana1583@gmail.com>
2022-04-08 15:57:30 +0930

    sdfds

--- cart.cpp
+++ cart.cpp
@@ -7,12 +7,9 @@ cart::cart()
     num_of_meerkats = 0;
 }
 
-bool flag = true;
-
 bool cart::addMeerkat(meerkat cat) {
     if (num_of_meerkats >= 4) {
-        flag = false;
-        return flag;
+        return false;
     }
     m_cart[num_of_meerkats] = cat;
     num_of_meerkats++;
@@ -24,9 +21,7 @@ void cart::emptyCart() {
 }
 
 void cart::printMeerkats() {
-    if (flag == true) {
-        for (int i = 0; i < num_of_meerkats; i++) {
-            cout << m_cart[i].getName() << " " << m_cart[i].getAge() << endl;
-        }
+    for (int i = 0; i < num_of_meerkats; i++) {
+        cout << m_cart[i].getName() << " " << m_cart[i].getAge() << endl;
     }
 }
