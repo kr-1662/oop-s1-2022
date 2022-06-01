227b0e9c872b9f31f821b7250695bb2dd48adce6
kr-1662 <a1834181@adelaide.edu.au>
2022-06-01 11:59:24 +0930

    fsdfs

--- sort_by_name.cpp
+++ sort_by_name.cpp
@@ -4,7 +4,7 @@ void sort_by_name:: sort(animal **animals, int n)
 {
     int i,j;
     animal *temp;
-    for (i = 0; i < n-1; i++)
+    for (i = 0; i < n; i++)
     {
         for (j = 0; j < n-i-1; j++)
         {
