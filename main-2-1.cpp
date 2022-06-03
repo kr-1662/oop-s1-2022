f661579d10f1ca76501eb2ca9f41b02faaa53433
kr-1662 <a1834181@adelaide.edu.au>
2022-06-03 16:41:38 +0930

    prac-exam-4

--- main-2-1.cpp
+++ main-2-1.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main() {
     FoodPackage fp("Bus", 10);
 
-    cout << fp.get_cargotype() << " " << fp.get_units() << " " << fp.get_transport() << endl;
+    cout << fp.get_cargoType() << " " << fp.get_units() << " " << fp.get_transport() << endl;
 
     return 0;
 }
