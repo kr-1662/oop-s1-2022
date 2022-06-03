9713b3d0a628c4d28b3652dea4aa51f88ee485ff
kr-1662 <a1834181@adelaide.edu.au>
2022-06-03 16:51:20 +0930

    prac-exam-4

--- Consignment.cpp
+++ Consignment.cpp
@@ -3,15 +3,11 @@
 Consignment::Consignment()
 {
     csc = new Cargo * [5];
-    for (int i = 0; i < 5; i++) {
-        if (i == 0 || i == 4) {
-            csc[i] = new Fruit();
-        }
-
-        else {
-            csc[i] = new FoodPackage();
-        }
-    }
+    csc[0] = new Fruit("Train",1000);
+    csc[1] = new FoodPackage("Automobile",600);
+    csc[2] = new FoodPackage("Truck",100);
+    csc[3] = new FoodPackage("Train",501);
+    csc[4] = new Fruit("Plane", 500);
 }
 
 Cargo ** Consignment::get_cargo() {
